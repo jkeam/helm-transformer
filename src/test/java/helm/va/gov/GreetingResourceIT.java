2861e4fab3328c3ea4b804862b88fdaf9ab02dc6
Jon Keam <jpkeam@gmail.com>
2023-12-09 09:39:54 -0500

    initial commit

---
+++ src/test/java/helm/va/gov/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package helm.va.gov;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
