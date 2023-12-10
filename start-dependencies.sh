080169c0be32b580de2e8a8c868edd56387dad70
Jon Keam <jpkeam@gmail.com>
2023-12-09 19:50:07 -0500

    remove echo

--- start-dependencies.sh
+++ start-dependencies.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-podman run -ti --rm -p 2222:2222 \
+podman run --rm --name sftp -p 2222:2222 \
 -e PASSWORD_ACCESS=true \
 -e USER_NAME=ftpuser \
 -e USER_PASSWORD=ftppassword \
 -e DOCKER_MODS=linuxserver/mods:openssh-server-openssh-client \
-linuxserver/openssh-server
-
-echo 'started ftp!'
+-t linuxserver/openssh-server
